18342f1340119a72b7a8317e6f6aa257bc25c269
Restyled.io <commits@restyled.io>
2022-04-13 23:36:05 +0000

    Restyled by clang-format

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,29 @@
 const path = require("path");
 const TSLintPlugin = require('tslint-webpack-plugin');
 
-module.exports = [
-  {
-    entry: "./src/main.ts",
-    mode: "development",
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: "main.js",
-      library: "$",
-      libraryTarget: "umd"
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts'],
-    },
-    plugins: [
-      new TSLintPlugin({
-        files: ['./src/**/*.ts']
-      })
+module.exports = [ {
+  entry : "./src/main.ts",
+  mode : "development",
+  output : {
+    path : path.join(__dirname, "dist"),
+    filename : "main.js",
+    library : "$",
+    libraryTarget : "umd"
+  },
+  module : {
+    rules : [
+      {
+        test : /\.ts$/,
+        exclude : /node_modules/,
+        loader : "babel-loader",
+      },
     ],
-    node: {
-      __dirname: false,
-    }
+  },
+  resolve : {
+    extensions : [ '.ts' ],
+  },
+  plugins : [ new TSLintPlugin({files : [ './src/**/*.ts' ]}) ],
+  node : {
+    __dirname : false,
   }
-];
+} ];
