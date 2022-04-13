7b680389f1cd49e8bfdf0b823b01bac7572cabd8
Restyled.io <commits@restyled.io>
2022-04-13 23:36:09 +0000

    Restyled by prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require("path");
-const TSLintPlugin = require('tslint-webpack-plugin');
+const TSLintPlugin = require("tslint-webpack-plugin");
 
-module.exports = [ {
-  entry : "./src/main.ts",
-  mode : "development",
-  output : {
-    path : path.join(__dirname, "dist"),
-    filename : "main.js",
-    library : "$",
-    libraryTarget : "umd"
+module.exports = [
+  {
+    entry: "./src/main.ts",
+    mode: "development",
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "main.js",
+      library: "$",
+      libraryTarget: "umd",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts"],
+    },
+    plugins: [new TSLintPlugin({ files: ["./src/**/*.ts"] })],
+    node: {
+      __dirname: false,
+    },
   },
-  module : {
-    rules : [
-      {
-        test : /\.ts$/,
-        exclude : /node_modules/,
-        loader : "babel-loader",
-      },
-    ],
-  },
-  resolve : {
-    extensions : [ '.ts' ],
-  },
-  plugins : [ new TSLintPlugin({files : [ './src/**/*.ts' ]}) ],
-  node : {
-    __dirname : false,
-  }
-} ];
+];
