31995bf044cbbef1588203686da86bc85146cdb4
Frankeo <ingfrancisco.moreno1@gmail.com>
2022-04-27 21:39:34 +0000

    Add some unit testing

--- src/trie/trie.test.ts
+++ src/trie/trie.test.ts
@@ -1,9 +1,61 @@
 import assert from 'assert';
+import { Trie } from './trie';
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+describe('Trie', () => {
+
+  it('should return true when searching for a word that was inserted before', () => {
+    const trie = new Trie();
+    const key = "KEY";
+    trie.insert(key);
+
+    assert.ok(trie.search(key));
+  });
+
+  it('should return false for a word that was NOT inserted before', () => {
+    const trie = new Trie();
+    const key = "KEY";
+    const anotherKey = "NOT";
+    trie.insert(key);
+
+    assert.equal(trie.search(anotherKey), false);
+  });
+
+  it('should return false for a prefix of a word that was inserted before', () => {
+    const trie = new Trie();
+    const key = "KEY";
+    const prefix = "KE";
+    trie.insert(key);
+
+    assert.equal(trie.search(prefix), false);
+  });
+
+  it('should return true for a word that was inserted before among other words', () => {
+    const key = "KEY";
+    const words = [key, "RANDOM", "BLA", "TEST", "NOT", "ETC", "KEYS"];
+    const trie = new Trie();
+    words.forEach(word => trie.insert(word));
+    
+
+    assert.ok(trie.search(key));
+  });
+
+
+  it('should return false for a word that was deleted before', () => {
+    const key = "KEY";
+    const trie = new Trie();
+    trie.insert(key);
+    trie.remove(key);
+
+    assert.equal(trie.search(key), false);
+  });
+
+  it('should return false for a word in a list that was deleted before', () => {
+    const key = "KEY";
+    const words = [key, "RANDOM", "BLA", "TEST", "NOT", "ETC", "KEYS"];
+    const trie = new Trie();
+    trie.insert(key);
+    trie.remove(key);
+
+    assert.equal(trie.search(key), false);
   });
 });
