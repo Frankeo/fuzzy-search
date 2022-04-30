48c89f5872d06f8c76182b57d4546786266ff38a
Restyled.io <commits@restyled.io>
2022-04-30 13:55:14 +0000

    Restyled by prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 const TSLintPlugin = require("tslint-webpack-plugin");
-const {merge} = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const targetConfig = (target) =>
-    require(`./webpack/${target}.config.js`)(target);
+  require(`./webpack/${target}.config.js`)(target);
 
-module.exports = ({target}) => {
+module.exports = ({ target }) => {
   global.__basedir = __dirname;
-  return merge({
-    mode : "production",
-    entry : "./src/analyzer.ts",
-    module : {
-      rules : [
-        {
-          test : /\.ts$/,
-          exclude : /node_modules/,
-          loader : "ts-loader",
-        },
-      ],
+  return merge(
+    {
+      mode: "production",
+      entry: "./src/analyzer.ts",
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            loader: "ts-loader",
+          },
+        ],
+      },
+      resolve: {
+        extensions: [".ts"],
+      },
+      plugins: [new TSLintPlugin({ files: "./src/**/*.ts" })],
     },
-    resolve : {
-      extensions : [ ".ts" ],
-    },
-    plugins : [ new TSLintPlugin({files : "./src/**/*.ts"}) ],
-  },
-               targetConfig(target));
+    targetConfig(target)
+  );
 };
