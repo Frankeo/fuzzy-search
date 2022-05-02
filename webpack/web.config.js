ae5b042b5486eca3d82f586ce2379414dcad1147
Restyled.io <commits@restyled.io>
2022-05-02 21:29:59 +0000

    Restyled by clang-format

--- webpack/web.config.js
+++ webpack/web.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = () => ({
-  target: "web",
-  output: {
-    path: path.join(__basedir, "dist"),
-    filename: "bundle.js",
-    library: "$",
-    libraryTarget: "umd"
+  target : "web",
+  output : {
+    path : path.join(__basedir, "dist"),
+    filename : "bundle.js",
+    library : "$",
+    libraryTarget : "umd"
   },
 });
